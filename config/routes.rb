027941f83f5aceb28d8901c3d30e494f97ee2429
YUSUKE MORITA <yukke.dwc.0516@gmail.com>
2020-12-31 11:29:22 +0900

    Add a Static Pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/about'
+  root 'application#hello'
+  get 'login', to:'sessions#new'
+  post 'login', to:'sessions#create'
+  delete 'logout', to:'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
