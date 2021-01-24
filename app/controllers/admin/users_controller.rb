4043b5fcba8dc83274e1a09368c1bfb0099a7d34
sawa060 <>
2021-01-24 21:02:07 +0900

    依頼いただいた内容を修正

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -8,39 +8,57 @@ before_action :admin_user
   end
 
   def show
-  	@user = User.find(params[:id])
+    @user = User.find(params[:id])
     @courses = Course.all
-    @section = Section.find(params[:id])
+    @checked_lectures = IsCompletedLecture.where(user_id: params[:id])
+    @checked = IsCompletedLecture.where(user_id: params[:id]).order(created_at: :desc).limit(1) # IsCompletedLecture をwhere で対象のユーザーに絞り込み、order(created_at: :desc)で新しい順に並び替え、limit で1つだけ取り出します
+  end
 
-    # コースを限定してユーザーのcreated_atを持ってきたい
-    # @lecture = IsCompletedLecture.where(lecture_id: params[:lecture_id])
-    # @checked = IsCompletedLecture.where(user_id: params[:id],course_id: @lecture.section.course_id).order(created_at: :desc).limit(1)
+  def completed_lecture(course, user_id)
+    completed_lecture_count = 0
+    course.sections.each do |section|
+      completed_lectures = section.lectures.select{ |lecture| lecture.is_completed_lectures }
+      completed_lectures.each do |lecture|
+        completed_lecture_count += lecture.is_completed_lectures.where(user_id: user_id).length
+      end
+    end
+    return completed_lecture_count
+  end
 
-    @checked_lecture = IsCompletedLecture.where(lecture_id: params[:id]).order(created_at: :desc).limit(1)
-    @checked = IsCompletedLecture.where(user_id: params[:id]).order(created_at: :desc).limit(1)
-    # IsCompletedLecture をwhere で対象のユーザーに絞り込み、order(created_at: :desc)で新しい順に並び替え、limit で1つだけ取り出します
+  def latest_completed_lecture_date(course, user_id)
+    logger.debug('gggggggggggggggggg')
+    latest_completed_lectures = []
+    course.sections.each do |section|
+      section.lectures.each do |lecture|
+        if lecture.is_completed_lectures.present?
+          latest_completed_lectures.push(lecture.is_completed_lectures)
+        end
+      end
+    end
 
-
-
-    # ここがおかしいのか
-    # @course = Course.find(params[:id])
-    # @sections = @course.sections.all
-    # @section = Section.find(params[:id])
-    # @lectures = Lecture.all
+    if latest_completed_lectures.length > 0
+      latest_completed_lectures.each do |latest_completed_lecture|
+        if latest_completed_lecture.where(user_id: user_id).order(created_at: :desc).limit(1)[0]
+          return latest_completed_lecture.where(user_id: user_id).order(created_at: :desc).limit(1)[0].created_at
+        else
+          return '完了したレクチャーはまだありません'
+        end
+      end
+    end
   end
 
   def new
-  	@user = User.new
+    @user = User.new
   end
 
   def create
-  	@user = User.new(user_params)
-  	if @user.save
-  		flash[:success] = "登録に成功しました"
-  		redirect_to admin_users_url
-  	else
-  		render 'new'
-  	end
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "登録に成功しました"
+      redirect_to admin_users_url
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -65,17 +83,17 @@ before_action :admin_user
 
 
 private
-	def user_params
-		params.require(:user).permit(:name,:email,:employee_number,:password,:password_confirmation)
+  def user_params
+    params.require(:user).permit(:name,:email,:employee_number,:password,:password_confirmation)
     # params.permit(:name,:email,:employee_number,:password,:password_confirmation)
-	end
+  end
 
-	def logged_in_user
-	  unless logged_in?
-	    flash[:danger] = "ログインしてください"
-	    redirect_to login_url
-	  end
-	end
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "ログインしてください"
+      redirect_to login_url
+    end
+  end
 
     # 管理者かどうか確認
   def admin_user
@@ -89,5 +107,8 @@ private
     # redirect_to(root_url) unless current_user?(@user)
     # ここをアドミンでしかできないようにするには・・・どうする？
   end
+
+  helper_method :completed_lecture
+  helper_method :latest_completed_lecture_date
 
 end
